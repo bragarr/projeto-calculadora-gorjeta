3b868911dd6d07869289af4c24420765740cd7e3
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-21 22:05:16 -0400

    Atualização de arquivos para publicação

--- tip.js
+++ tip.js
@@ -9,38 +9,60 @@ const fifteenPercent = document.getElementById("ft");
 const twentyFivePercent = document.getElementById("tf");
 const fiftyPercent = document.getElementById("fift");
 const custom = document.getElementById("cus");
+const opcaoUsuario = document.querySelector(".opcao__user");
+const caixaCustom = document.querySelector(".custom");
 let resultTipPerPerson = document.querySelector(".per__person");
 let resultTotalPerPerson = document.querySelector(".per__total");
+let message = document.querySelector(".message");
 
+
+
+// Loop e Listener para identificar opção do usuário e executar calculo ou alerta de informação incompleta
 opcaoPorcentagem.forEach(botao => {
     botao.addEventListener("click", () => {
+        naoDeixaMaisDeUmBotaoSelecionado();
         if(botao.textContent === "5%") {
             document.getElementById("five").checked = true;
-            botao.classList.toggle("selected");
+            calculo();
+            botao.classList.add("selected");
         }
         if(botao.textContent === "10%") {
             document.getElementById("ten").checked = true;
-            botao.classList.toggle("selected");
+            calculo();
+            botao.classList.add("selected");
         }
         if(botao.textContent === "15%") {
             document.getElementById("ft").checked = true;
-            botao.classList.toggle("selected");
+            calculo();
+            botao.classList.add("selected");
         }
         if(botao.textContent === "25%") {
             document.getElementById("tf").checked = true;
-            botao.classList.toggle("selected");
+            calculo();
+            botao.classList.add("selected");
         }
         if(botao.textContent === "50%") {
             document.getElementById("fift").checked = true;
-            botao.classList.toggle("selected");
+            calculo();
+            botao.classList.add("selected");
         }
         if(botao.textContent === "") {
             document.getElementById("cus").checked = true;
+            calculo();
+            botao.classList.add("selected");
         }
     });
 })
 
+// Listener para realizar reset da calculadora quando usuário der click no botão reset
+botaoReset.addEventListener("click", () => resetCalculator());
+
+// Função para realizar cálcula da gorjeta
 function calculo() {
+    message.textContent = "";
+    bill.classList.remove("incorrect");
+    numberPeople.classList.remove("incorrect");
+    caixaCustom.classList.remove("incorrect");
     let valorConta = +bill.value;
     let qtyPessoas = +numberPeople.value;
     if(fivePercent.checked) {    
@@ -73,15 +95,42 @@ function calculo() {
         resultTipPerPerson.textContent = `$${calculoPorPessoa}`;
         resultTotalPerPerson.textContent = `$${calculoTotal}`;
     }
-    if(custom.checked) {
-        let calculoPorPessoa = ((valorConta*(custom.dataset.value))/qtyPessoas).toFixed(2); 
-        let calculoTotal =  ((valorConta*(custom.dataset.value) + valorConta)/qtyPessoas).toFixed(2);
+    if(custom.checked && opcaoUsuario.value !== "0") {
+        checkCaixaCustom();
+        let calculoPorPessoa = ((valorConta*(Number(opcaoUsuario.value)/100))/qtyPessoas).toFixed(2); 
+        let calculoTotal =  ((valorConta*((Number(opcaoUsuario.value)/100)) + valorConta)/qtyPessoas).toFixed(2);
         resultTipPerPerson.textContent = `$${calculoPorPessoa}`;
         resultTotalPerPerson.textContent = `$${calculoTotal}`;
     }
+    if(numberPeople.value === "0" ||
+        numberPeople.value === ""
+    ) 
+    {
+        message.textContent = "can't be zero";
+        resultTipPerPerson.textContent = "$0.00";
+        resultTotalPerPerson.textContent = "$0.00";
+        bill.classList.add("incorrect");
+        numberPeople.classList.add("incorrect");
+    }
+    if(opcaoUsuario.value === "0" &&
+        custom.checked === true ||
+        opcaoUsuario.value ==="" &&
+        custom.checked === true 
+    ) 
+    {
+        message.textContent = "can't be zero";
+        resultTipPerPerson.textContent = "$0.00";
+        resultTotalPerPerson.textContent = "$0.00";
+    }
 }
 
+// Função para limpar toda a calculadora
 function resetCalculator() {
+    naoDeixaMaisDeUmBotaoSelecionado();
+    message.textContent = "";
+    bill.classList.remove("incorrect");
+    numberPeople.classList.remove("incorrect");
+    caixaCustom.classList.remove("incorrect");
     bill.value = "";
     numberPeople.value = "";
     document.getElementById("five").checked = false;
@@ -90,7 +139,32 @@ function resetCalculator() {
     document.getElementById("tf").checked = false;
     document.getElementById("fift").checked = false;
     document.getElementById("cus").checked = false;
+    resultTipPerPerson.textContent = "$0.00";
+    resultTotalPerPerson.textContent = "$0.00";
 }
 
-botaoReset.addEventListener("click", () => resetCalculator());
+// Função para checar a opção manual feita pelo usuário
+function checkCaixaCustom() {
+    selecaoOpcao.forEach(botao => {
+        if(botao.textContent === "") {
+            if(botao.checked === true && opcaoUsuario.vallue ==="0"||botao.checked === true && opcaoUsuario.value ==="") {
+            caixaCustom.classList.add("incorrect");
+            }   
+        } 
+    })
+}
 
+selecaoOpcao.forEach(botao => {
+    if(botao.checked === true) {
+        document.addEventListener("change", calculo());
+    }
+})
+
+
+function naoDeixaMaisDeUmBotaoSelecionado() {
+    opcaoPorcentagem.forEach(botao => {
+        if(botao.classList.contains("selected")) {
+            botao.classList.remove("selected");
+        }
+    })
+}
